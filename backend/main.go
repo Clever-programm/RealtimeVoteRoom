b175aaad6f3e6e597678b4f35a87e589312b7d7e
i.tsaregorodtsev <tsaregorodtsevigorek@yandex.ru>
2026-07-09 16:12:14 +0400

    feat(backend): connect database

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Option struct {
@@ -20,52 +22,73 @@ type Poll struct {
 	Options  []Option `json:"options"`
 }
 
-var mockPoll = Poll{
-	ID:       1,
-	Question: "Какой стек выбрать для реалтайм пет-проекта?",
-	Options: []Option{
-		{ID: 1, Text: "Go + Flutter + Postgres", Votes: 10},
-		{ID: 2, Text: "Node.js + React + MongoDB", Votes: 3},
-		{ID: 3, Text: "Python + Vue + SQLite", Votes: 5},
-	},
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	slog.Info("Input request",
-		"method", r.Method,
-		"path", r.URL.Path,
-		"remote_addr", r.RemoteAddr,
-	)
-
-	fmt.Fprintf(w, "It is backend for realtime vote room.")
-}
+var dbPool *pgxpool.Pool
 
 func getPollHandler(w http.ResponseWriter, r *http.Request) {
-	slog.Info("Запрос данных опроса", "method", r.Method, "path", r.URL.Path)
-
 	w.Header().Set("Content-Type", "application/json")
 
-	err := json.NewEncoder(w).Encode(mockPoll)
+	ctx := context.Background()
+
+	var poll Poll
+
+	err := dbPool.QueryRow(ctx, "SELECT id, question FROM polls WHERE id = $1", 1).Scan(&poll.ID, &poll.Question)
 	if err != nil {
-		slog.Error("Ошибка кодирования JSON", "error", err)
-		http.Error(w, "Внутренняя ошибка сервера", http.StatusInternalServerError)
+		slog.Error("Getting poll database error", "error", err)
+		http.Error(w, "Database error", http.StatusInternalServerError)
 		return
 	}
+
+	rows, err := dbPool.Query(ctx, "SELECT id, text, votes FROM options WHERE poll_id = $1 ORDER BY id", poll.ID)
+	if err != nil {
+		slog.Error("Getting option database error", "error", err)
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var opt Option
+		if err := rows.Scan(&opt.ID, &opt.Text, &opt.Votes); err != nil {
+			slog.Error("Scan option row error", "error", err)
+			continue
+		}
+		poll.Options = append(poll.Options, opt)
+	}
+
+	json.NewEncoder(w).Encode(poll)
 }
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
-	http.HandleFunc("/", homeHandler)
+	connStr := "postgres://myuser:mypassword@localhost:5433/mydb?sslmode=disable"
+
+	ctx := context.Background()
+	var err error
+
+	dbPool, err = pgxpool.New(ctx, connStr)
+	if err != nil {
+		slog.Error("Unable to create connection pool", "error", err)
+		os.Exit(1)
+	}
+	defer dbPool.Close()
+
+	err = dbPool.Ping(ctx)
+	if err != nil {
+		slog.Error("Database is not available (Ping failed)", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("Successful connection to PostgreSQL")
+
 	http.HandleFunc("/api/poll", getPollHandler)
 
 	port := ":8080"
-	slog.Info("Server successfully started", "port", port)
+	slog.Info("Server successfully start", "port", port)
 
-	err := http.ListenAndServe(port, nil)
+	err = http.ListenAndServe(port, nil)
 	if err != nil {
-		slog.Error("Critical server running error", "error", err)
+		slog.Error("Critical server start error", "error", err)
 		os.Exit(1)
 	}
 }
